95bced7f2987e9e3f1afe28f265dff8985ccf9ff
VictorLuigi27 <victor.diaz@oclock.school>
2025-02-25 18:28:42 +0100

    change design header

--- src/assets/components/Header/header.tsx
+++ src/assets/components/Header/header.tsx
@@ -1,22 +1,26 @@
 export default function Header() {
-    return (
-      <div className="bg-blue-950 text-white p-4 border-b-3 border-t-blue-600 flex items-center">
+  return (
+    <div className="bg-blue-950 text-white p-4 border-b-[3px] border-blue-600 shadow-lg pr-[4.5rem]">
+      <div className="max-w-screen-lg mx-auto flex items-center justify-between">
         
+        {/* Logo */}
         <h1 className="text-4xl font-bold">PIXEL</h1>
-  
+
+        {/* Barre de recherche */}
         <div className="flex-1 flex justify-center">
           <input
             type="text"
             placeholder="Rechercher..."
-            className="w-64 p-2 bg-transparent border-2 border-cyan-400 text-white rounded-full outline-none focus:ring-2 focus:ring-cyan-500 transition duration-300 placeholder-gray-400"
+            className="w-[15rem] p-2 bg-transparent border-2 border-cyan-400 text-white rounded-full outline-none focus:ring-2 focus:ring-cyan-500 transition duration-300 placeholder-gray-400"
           />
         </div>
-  
-        <button className="bg-green-600 hover:bg-green-500 text-white p-3 rounded-full flex items-center justify-center transition duration-300 cursor-pointer">
+
+        {/* Bouton Ajout */}
+        <button className="bg-green-600 hover:bg-green-500 text-white p-3 rounded-full transition duration-300 shadow-md flex items-center justify-center">
           <span className="text-2xl font-bold">+</span>
         </button>
-        
+
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
